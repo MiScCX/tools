18be131a637a68755bf1e029823cc2496dbde895
Michael Schindler <35303768+MiScCX@users.noreply.github.com>
2018-02-16 15:49:23 +0100

    Update install_homebrew.sh

--- macosx/install_homebrew.sh
+++ macosx/install_homebrew.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Install Homebew Script"
+echo "Install Homebrew Script"
 echo "M. Schindler"
 echo
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
